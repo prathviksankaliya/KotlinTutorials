1b3c2678ccb7c6aae966daf74b580915afd27a8e
Shadow <prathviksankaliya@gmail.com>
2023-08-06 16:24:37 +0530

    Operators and Variables Added

---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,29 @@
+fun main() {
+    var a = 13
+    var b = 2
+
+    // Arithmetic Operators
+    println(a + b) // 15
+    println(a - b) // 11
+    println(a / b) // 6
+    println(a.toFloat() / b) // 6.5
+    println(a * b) // 26
+    println(a % b) // 1
+
+    // Relational Operators
+    println(a < b)
+    println(a > b)
+    println(a <= b)
+    println(a >= b)
+    println(a == b)
+    println(a != b)
+    println(a === b)
+
+
+    val x = 10
+    val y = x
+
+    println()
+    println(x == y)
+    println(x === y)
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,12 @@
+// var use for variable declaration and it/'s type depend on who's first element is stored
+// val means it's constant variable just one time holds value
+
+fun main() {
+//    var score = 5
+//    score = 10
+//    print(score)
+
+    val hello = "hii"
+//    hello = "bye"
+    print(hello)
+}
